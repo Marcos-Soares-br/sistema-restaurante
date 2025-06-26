4fd3428e7eb5284296d78fe90a0200de66dd03e0
Marcos-Soares-br <marcosantoinioss@gmail.com>
2025-06-25 21:51:09 -0300

    resolução do erro cors teste 1

--- backend/api/exibirCardapio.js
+++ backend/api/exibirCardapio.js
@@ -1,22 +1,19 @@
 import corsMiddleware from '../corsConfig.js';
-import { exibirCardapio } from '../models/dbModel.js'; 
+import { exibirCardapio } from '../models/dbModel.js';
 
 // Função Serverless para a rota "Exibir Cardapio"
 export default async function handler(req, res) {
-
-  corsMiddleware(req, res, () => {});
-
-  if(req.method == 'GET') {
-    try {
+  corsMiddleware(req, res, async () => {
+    if (req.method === 'GET') {
+      try {
         const cardapio = await exibirCardapio();
-        res.status(200).json(cardapio);
-        
+        return res.status(200).json(cardapio);  
       } catch (error) {
         console.error('Erro ao exibir cardápio:', error);
-        res.status(500).json({ error: 'Erro ao exibir cardápio', details: error.message });
+        return res.status(500).json({ error: 'Erro ao exibir cardápio', details: error.message });
       }
-  } else {
-    return res.status(405).json({ mensagem: 'Método não permitido!' });
-  }
-
+    } else {
+      return res.status(405).json({ mensagem: 'Método não permitido!' });
+    }
+  });
 }
