911d98fbb906426034f4b3e64315461464a0801d
Marcos-Soares-br <marcosantoinioss@gmail.com>
2025-06-28 02:34:38 -0300

    Migrando api para railway 2

--- backend/api/exibirCardapio.js
+++ backend/api/exibirCardapio.js
@@ -1,21 +1,19 @@
 import corsMiddleware from '../corsConfig.js';
-// import { exibirCardapio } from '../models/dbModel.js';
+import { exibirCardapio } from '../models/dbModel.js';
 
 // Função Serverless para a rota "Exibir Cardapio"
 export default async function handler(req, res) {
   corsMiddleware(req, res, async () => {
-  //   if (req.method === 'GET') {
-  //     try {
-  //       const cardapio = await exibirCardapio();
-  //       return res.status(200).json(cardapio);  
-  //     } catch (error) {
-  //       console.error('Erro ao exibir cardápio:', error);
-  //       return res.status(500).json({ error: 'Erro ao exibir cardápio', details: error.message });
-  //     }
-  //   } else {
-  //     return res.status(405).json({ mensagem: 'Método não permitido!' });
-  //   }
-  const resposta = {Mensagem: "CR7 >>> messi"}
-  return res.status(200).json(resposta);
+    if (req.method === 'GET') {
+      try {
+        const cardapio = await exibirCardapio();
+        return res.status(200).json(cardapio);  
+      } catch (error) {
+        console.error('Erro ao exibir cardápio:', error);
+        return res.status(500).json({ error: 'Erro ao exibir cardápio', details: error.message });
+      }
+    } else {
+      return res.status(405).json({ mensagem: 'Método não permitido!' });
+    }
    });
 }
